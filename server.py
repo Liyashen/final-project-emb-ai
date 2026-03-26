3d3ac94167ca5f937e40886773db614f1b4d199c
Liyashen <liyashen1@gmail.com>
2026-03-26 01:54:45 -0400

    server.py final update

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+""" 
+This module receives input from users and calls emotion_detector function to analyze the input and 
+lastly returns the response back to the user
+
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +11,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_index_page():
+    """Render the index.html page"""
     return render_template('index.html')
 
+
 @app.route('/emotionDetector')
-def emotionDetector():
+def emotion_detect():
+    """Send request to emotion_detector function and get a response back"""
+    # Retrieve the text to analyze from the request arguments
     text = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text)
+
+    # Check if the dominant_emotion is None, indicating an error or invalid input if it is None
     if response['dominant_emotion'] is None:
         return {'message':'Invalid text! Please try again'}
-        
-    
+    # If the dominant_emotion is not None, extract emotions and dominant emotion from the response
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
